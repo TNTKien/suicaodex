ed379e3746f99070eb278b969d034f215f701ded
iamneyk <iamneyk@gmail.com>
2024-10-09 02:16:16 +0700

    đ

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
     <html suppressHydrationWarning={true} lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link ref="og:image" href="/matologo.png" />
+        <link rel="og:image" href="/matologo.png" />
         <meta
           name="google-site-verification"
           content="22zmW8vLmhPSRHgJo2Yg_J0w09A-1ZlmJjFG6ldXdjA"
