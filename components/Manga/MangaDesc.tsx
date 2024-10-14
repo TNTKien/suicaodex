54b116db68e43301e5732f48caea252fea705b12
iamneyk <iamneyk@gmail.com>
2024-10-14 18:18:17 +0700

    fix

--- components/Manga/MangaDesc.tsx
+++ components/Manga/MangaDesc.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
 interface MangaDescProps {
-    desc: string;
+  desc: string;
 }
 
 const MangaDesc = ({ desc }: MangaDescProps) => {
@@ -13,19 +13,11 @@ const MangaDesc = ({ desc }: MangaDescProps) => {
   const lines = desc.split("\n");
   const displayLines = showMore ? lines : lines.slice(0, maxLines);
 
-  // const renderers = {
-  //   a: ({ href, children }: { href?: string, children: React.ReactNode }) => (
-  //     <a href={href} style={{ textDecoration: 'underline' }}>
-  //       {children}
-  //     </a>
-  //   )
-  // };
-
   return (
     <div className='mb-4'>
       <ReactMarkdown
         className="text-sm"
-        remarkPlugins={[[remarkGfm, {singleTilde: false}]]}
+        remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
         components={
           {
             a: ({ href, children }) => (
@@ -35,12 +27,12 @@ const MangaDesc = ({ desc }: MangaDescProps) => {
             )
           }
         }>
-          {displayLines.join("\n")}
+        {displayLines.join("\n")}
       </ReactMarkdown>
       {lines.length > maxLines && (
-        <span onClick={() => setShowMore(!showMore)} className="text-sm text-[#f31260] cursor-pointer">
+        <button onClick={() => setShowMore(!showMore)} className="text-sm text-[#f31260] cursor-pointer">
           {showMore ? "Thu gọn" : " Xem thêm..."}
-        </span>
+        </button>
       )}
 
     </div>
