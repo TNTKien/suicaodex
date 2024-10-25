952768960dd624663488dab0bdf564ca29ec9ef8
iamneyk <iamneyk@gmail.com>
2024-10-26 00:23:47 +0700

    no

--- components/Search/SearchSection.tsx
+++ components/Search/SearchSection.tsx
@@ -60,7 +60,7 @@ const SearchSection = () => {
               <ModalHeader className="grid grid-cols-1 gap-3 border-b-1">
                 <h2 className="text-2xl font-bold">Tìm kiếm</h2>
                 <Input
-                  autoFocus
+                  autoFocus // eslint-disable-line jsx-a11y/no-autofocus
                   placeholder="Nhập từ khoá..."
                   variant="bordered"
                   onChange={handleSearch}
