c9c43258e4f97213ae33e35c8fbcb017110038b5
iamneyk <iamneyk@gmail.com>
2024-10-29 05:25:20 +0700

    add tooltip

--- components/Session/SignIn.tsx
+++ components/Session/SignIn.tsx
@@ -4,19 +4,28 @@ import { signIn, useSession } from "next-auth/react";
 import { Button } from "@nextui-org/button";
 import { LogInIcon } from "lucide-react";
 import { UserNav } from "./UserNav";
+import { Tooltip } from "@nextui-org/react";
 
 export function SignIn() {
   const { data: session } = useSession();
   if (!session)
     return (
-      <Button
-        variant="flat"
-        color="primary"
-        isIconOnly
-        onPress={() => signIn("discord")}
+      <Tooltip
+        content="Đăng nhập bằng Discord"
+        size="sm"
+        placement="bottom-end"
+        radius="sm"
+        closeDelay={200}
       >
-        <LogInIcon />
-      </Button>
+        <Button
+          variant="flat"
+          color="primary"
+          isIconOnly
+          onPress={() => signIn("discord")}
+        >
+          <LogInIcon />
+        </Button>
+      </Tooltip>
     );
   return <UserNav />;
 }
