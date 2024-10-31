929da7ef31f2f2346817cc671d416c6a640673c9
iamneyk <iamneyk@gmail.com>
2024-10-31 15:37:40 +0700

    it worked btw

--- components/Chapter/ChapterTable/ChapterVolume.tsx
+++ components/Chapter/ChapterTable/ChapterVolume.tsx
@@ -13,69 +13,61 @@ import { ChapterCard } from "./ChapterCard";
 import { ListTree, ListX } from "lucide-react";
 import NotFoundImg from "@/public/404.png";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 interface ChapterVolumeProps {
   volume: Volume[];
 }
 
 export const ChapterVolume = ({ volume }: ChapterVolumeProps) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    setIsLoaded(true);
-  }, []);
-
-  if (!isLoaded) return null;
-
-  if (volume.length === 0)
-    return (
-      <Card className="w-full rounded-lg">
-        <CardHeader className="flex gap-3">
-          <Button
-            fullWidth
-            radius="sm"
-            variant="faded"
-            color="danger"
-            className="font-semibold"
-            startContent={<ListX />}
-            isDisabled
-          >
-            Truyện chưa có chapter nào!
-          </Button>
-        </CardHeader>
-
-        <CardBody className="items-center justify-center">
-          <Image src={NotFoundImg} alt="not found" />
-        </CardBody>
-      </Card>
-    );
-
   return (
-    <Accordion
-      variant="shadow"
-      isCompact
-      selectionMode="multiple"
-      defaultExpandedKeys={["0", "1"]}
-      aria-label="Volume"
-      className="shadow-small rounded-lg"
-      itemClasses={{
-        heading: "font-semibold",
-      }}
-    >
-      {volume.map((vol, index) => (
-        <AccordionItem
-          startContent={vol.vol === null ? <ListX /> : <ListTree />}
-          key={index}
-          title={vol.vol === null ? "No Volume" : `Volume ${vol.vol}`}
-          textValue="Volume"
+    <>
+      {volume.length === 0 ? (
+        <Card className="w-full rounded-lg">
+          <CardHeader className="flex gap-3">
+            <Button
+              fullWidth
+              radius="sm"
+              variant="faded"
+              color="danger"
+              className="font-semibold"
+              startContent={<ListX />}
+              isDisabled
+            >
+              Truyện chưa có chapter nào!
+            </Button>
+          </CardHeader>
+
+          <CardBody className="items-center justify-center">
+            <Image src={NotFoundImg} alt="not found" />
+          </CardBody>
+        </Card>
+      ) : (
+        <Accordion
+          variant="shadow"
+          isCompact
+          selectionMode="multiple"
+          defaultExpandedKeys={["0", "1"]}
           aria-label="Volume"
+          className="shadow-small rounded-lg"
+          itemClasses={{
+            heading: "font-semibold",
+          }}
         >
-          {vol.chapters.map((chapter) => (
-            <ChapterCard key={chapter.chapter} chapters={chapter} />
+          {volume.map((vol, index) => (
+            <AccordionItem
+              startContent={vol.vol === null ? <ListX /> : <ListTree />}
+              key={index}
+              title={vol.vol === null ? "No Volume" : `Volume ${vol.vol}`}
+              textValue="Volume"
+              aria-label="Volume"
+            >
+              {vol.chapters.map((chapter) => (
+                <ChapterCard key={chapter.chapter} chapters={chapter} />
+              ))}
+            </AccordionItem>
           ))}
-        </AccordionItem>
-      ))}
-    </Accordion>
+        </Accordion>
+      )}
+    </>
   );
 };
