2f1296b838ab811439dc10173faec09eb9e44b84
iamneyk <iamneyk@gmail.com>
2024-11-08 11:24:11 +0700

    fix skeleton

--- components/Manga/Carousel/MangaCarouselSkeleton.tsx
+++ components/Manga/Carousel/MangaCarouselSkeleton.tsx
@@ -4,13 +4,13 @@ export default function MangaCarouselSkeleton() {
   return (
     <Card
       isBlurred
-      className="border-none bg-background/60 dark:bg-default-100/50 w-full h-[324]"
+      className="border-none bg-background/60 dark:bg-default-100/50 w-full h-[324px]"
       shadow="sm"
     >
       <CardBody>
         <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-start justify-center">
           <div className="relative col-span-6 md:col-span-2">
-            <Skeleton className="h-[300] rounded-lg bg-default-300" />
+            <Skeleton className="h-[200px] md:h-[300px] rounded-lg bg-default-300" />
           </div>
           <div className="flex flex-col col-span-6 md:col-span-8 gap-2">
             <div className="flex justify-between items-start">
