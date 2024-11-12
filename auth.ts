723a1728bc834eaff86d603a80926a79d97ed69f
iamneyk <iamneyk@gmail.com>
2024-11-12 14:55:27 +0700

    remove d1

--- auth.ts
+++ auth.ts
@@ -1,8 +1,6 @@
-import { D1Adapter } from "@auth/d1-adapter"
 import NextAuth from "next-auth"
 import Discord from "next-auth/providers/discord"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Discord],
-    //adapter: D1Adapter(process.env.DATABASE_ID),
 })
