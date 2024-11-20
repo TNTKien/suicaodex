52a74c2fc4ebf25ce192c1ac2714b7f0aeebfaab
iamneyk <iamneyk@gmail.com>
2024-11-21 01:27:59 +0700

    add metadata generation for in-progress page

--- app/in-progress/page.tsx
+++ app/in-progress/page.tsx
@@ -1,4 +1,13 @@
 import Incomming from "@/components/incoming";
+import { Metadata } from "next";
+
+export function generateMetadata(): Metadata {
+  return {
+    title: "Comming Soon - SuicaoDex",
+    description: "Tính năng đang phát triển",
+    keywords: ["Mới cập nhật", "Manga"],
+  };
+}
 
 export default function Page() {
   return <Incomming />;
