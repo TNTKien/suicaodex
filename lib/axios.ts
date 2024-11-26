7fb9b90f4a4a670d83a102c1c8a163d0f534572e
iamneyk <iamneyk@gmail.com>
2024-11-27 02:28:39 +0700

    fix: update base URL environment variable to NEXT_PUBLIC_BASE_URL

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
 import { siteConfig } from "@/config/site";
 import axios from "axios";
 
-const NEXT_BASE_URL = process.env.NEXT_BASE_URL || "http://localhost:3000";
+const NEXT_BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 const axiosInstance = axios.create({
   baseURL: NEXT_BASE_URL + "/api/proxy?url=" + siteConfig.mangadexAPI.baseURL,
