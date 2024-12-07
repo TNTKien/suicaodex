11174aca2a806fd3461d959773e787d977a694cf
iamneyk <iamneyk@gmail.com>
2024-12-07 14:15:03 +0700

    fix

--- components/Comment/NoComment.tsx
+++ components/Comment/NoComment.tsx
@@ -5,7 +5,12 @@ import { Image } from "@nextui-org/react";
 export default function NoComment() {
   return (
     <div className="flex flex-col gap-2 items-center justify-center text-lg text-gray-500 p-10">
-      <Image alt="No comment" src="/incoming.png" />
+      <Image
+        alt="No comment"
+        src="/incoming.png"
+        disableSkeleton
+        loading="eager"
+      />
       <p>Tính năng đang phát triển...</p>
     </div>
   );
